540312c8dc824e0aa85de89d4242bd9b0d33608b
kellel14 <kellel14@gmail.com>
2019-06-20 14:39:45 -0400

    first push

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        int[] testArray = new int [10];
+        testArray [0] = 2;
+        int[] testArray2 = new int[] {2,3,4,5};
+        for (int x = 0; x < testArray2.length; x++){
+            System.out.println(testArray[x]);
+        }
+        for (int y = 0; y < testArray2.length; y++){
+            System.out.println(testArray2[y]);
+        }
+
+
+
+    }
+}
